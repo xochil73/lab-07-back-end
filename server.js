d1300e639dfc4392126fd54c298ec7c87f207003
Karl Polintan <kf.polntan@gmail.com>
2018-12-08 16:13:21 -0800

    Start, finished Feat1

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 //Application Dependencies
 const express = require('express');
 const cors = require('cors');
+const superAgent = require('superagent');
 const weatherArray = [];
 
 //Load env vars;
@@ -22,22 +23,48 @@ app.get('/location', (request, response) => {
 
 // Get weather data
 app.get('/weather', (request, response) => {
-  const weatherGet = searchWeather(request.query.data || 'Lynnwood, WA');
-  response.send(weatherGet);
+  searchWeather(request.query.data || 'Lynnwood, WA')
+    .then(weatherData => {
+      response.send(weatherData);
+    })
+  // console.log(weatherGet);
 })
 
+// from class
 function searchToLatLong(query){
-  const geoData = require('./data/geo.json');
-  const location = new Location(geoData.results[0]);
-  return location;
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
+  return superAgent.get(url)
+    .then(geoData => {
+      const location = new Location(geoData.body.results[0]);
+      return location;
+    })
+    .catch(err => console.error(err));
+  
 }
 
 function searchWeather(query){
-  const weatherData = require('./data/darksky.json');
-  for (let i in weatherData.daily.data){
-    const forecast = new Forecast(weatherData.daily.data[i]);
-  }
-  return weatherArray;
+  const url = `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/47.85356789999999,-122.34211`;
+  // console.log(url);
+  // console.log(superAgent.get(url));
+  return superAgent.get(url)
+    .then(weatherData => {
+      // console.log(weatherData.body.daily.data);
+      
+      let wArr = weatherData.body.daily.data.map( 
+          forecast => {
+            let data = {};
+            data.forecast = forecast.summary;
+            data.time = new Date(forecast.time * 1000).toDateString();
+            return data;
+          }
+        );
+        // for (let i in weatherData.daily.data){
+          //   const forecast = new Forecast(weatherData.daily.data[i]);
+      // }
+      console.log(wArr);
+      return wArr;
+    })
+    .catch(err => console.error(err));
 }
 
 function Location(location){
@@ -55,11 +82,11 @@ function Forecast(weather){
 
 // Error messages
 app.get('/*', function(req, res) {
-  res.status(404).send('you are in the wrong place');
+  res.status(404).send('halp, you are in the wrong place');
 })
 
 function errorMessage(res){
-  res.status(500).send('something went wrong');
+  res.status(500).send('something went wrong. plzfix.');
 } //created a function to handle the 500 errors but not sure what to do with it
 
 app.listen(PORT, () => {
